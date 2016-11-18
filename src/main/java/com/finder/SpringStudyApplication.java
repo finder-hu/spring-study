a5e0599292d93acf14e4a0d77e47603bef80ed30
longhuai.hlh <longhuai.hlh@alipay.com>
2016-11-18 13:33:25 +0800

    Init data access project using MYSQL database;

--- src/main/java/com/finder/SpringStudyApplication.java
+++ src/main/java/com/finder/SpringStudyApplication.java
@@ -1,12 +1,26 @@
 package com.finder;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class SpringStudyApplication {
+public class SpringStudyApplication implements CommandLineRunner {
+
+	@SuppressWarnings("SpringJavaAutowiringInspection")
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStudyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		String name = jdbcTemplate.queryForObject
+				("select name from test where idtest = 2",String.class);
+		System.out.println("name:" + name);
 	}
 }
